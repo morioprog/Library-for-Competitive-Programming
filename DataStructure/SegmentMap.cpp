29d9c823084b7a3edf2aa34073889c08d9ec8410
morioprog <morio.prog@gmail.com>
2019-11-27 20:31:26 +0900

    fix SegmentMap

--- DataStructure/SegmentMap.cpp
+++ DataStructure/SegmentMap.cpp
@@ -1,38 +1,38 @@
 class SegmentMap : public map<int_fast64_t, int_fast64_t> {
-	bool flagToMergeAdjacentSegment;
-	SegmentMap(bool flagToMergeAdjacentSegment) : flagToMergeAdjacentSegment(flagToMergeAdjacentSegment) {}
-	auto get(int_fast64_t p) const {
-		auto it = upper_bound(p);
-		if (it == begin() or (--it)->second < p) return end();
-		return it;
-	}
-	void insert(int_fast64_t l, int_fast64_t r) {
-		auto itl = upper_bound(l), itr = upper_bound(r + flagToMergeAdjacentSegment);
-		if (itl != begin()) {
-			if ((--itl)->second < l - flagToMergeAdjacentSegment) ++itl;
-		}
-		if (itl != itr) {
-			l = min(l, itl->first);
-			r = max(r, prev(itr)->second);
-			erase(itl, itr);
-		}
-		(*this)[l] = r;
-	}
-	void remove(int_fast64_t l, int_fast64_t r) {
-		auto itl = upper_bound(l), itr = upper_bound(r);
-		if (itl != begin()) {
-			if ((--itl)->second < l) ++itl;
-		}
-		if (itl == itr) return;
-		int tl = min(l, itl->first), tr = max(r, prev(itr)->second);
-		erase(itl, itr);
-		if (tl < l) (*this)[tl] = l - 1;
-		if (r < tr) (*this)[r + 1] = tr;
-	}
-	bool same(int_fast64_t p, int_fast64_t q) const {
-		const auto&& it = get(p);
-		return it != end() and it->first <= q and q <= it->second;
-	}
+    bool flagToMergeAdjacentSegment;
+    SegmentMap(bool flagToMergeAdjacentSegment) : flagToMergeAdjacentSegment(flagToMergeAdjacentSegment) {}
+    auto get(int_fast64_t p) const {
+        auto it = upper_bound(p);
+        if (it == begin() or (--it)->second < p) return end();
+        return it;
+    }
+    void insert(int_fast64_t l, int_fast64_t r) {
+        auto itl = upper_bound(l), itr = upper_bound(r + flagToMergeAdjacentSegment);
+        if (itl != begin()) {
+            if ((--itl)->second < l - flagToMergeAdjacentSegment) ++itl;
+        }
+        if (itl != itr) {
+            l = min(l, itl->first);
+            r = max(r, prev(itr)->second);
+            erase(itl, itr);
+        }
+        (*this)[l] = r;
+    }
+    void remove(int_fast64_t l, int_fast64_t r) {
+        auto itl = upper_bound(l), itr = upper_bound(r);
+        if (itl != begin()) {
+            if ((--itl)->second < l) ++itl;
+        }
+        if (itl == itr) return;
+        int tl = min(l, itl->first), tr = max(r, prev(itr)->second);
+        erase(itl, itr);
+        if (tl < l) (*this)[tl] = l - 1;
+        if (r < tr) (*this)[r + 1] = tr;
+    }
+    bool same(int_fast64_t p, int_fast64_t q) const {
+        const auto&& it = get(p);
+        return it != end() and it->first <= q and q <= it->second;
+    }
 };
 
 /*
