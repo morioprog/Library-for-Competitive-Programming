3f88847402e5b11a2afee291e1a013754e1f00ea
morioprog <morio.prog@gmail.com>
2020-01-31 01:14:18 +0900

    fix template

--- Template/Main.cpp
+++ Template/Main.cpp
@@ -1,90 +1,103 @@
 #include <bits/stdc++.h>
 using namespace std;
-//#define int long long
+// #define int long long
+// #define endl '\n'
+
 //TEMPLATE START---------------8<---------------8<---------------8<---------------8<---------------//
-typedef long long ll;       typedef long double ld;  typedef pair<int,int> pii; typedef pair<ll,ll> pll;  typedef vector<int> vi;   typedef vector<ll> vl;
-typedef vector<string> vst; typedef vector<bool> vb; typedef vector<ld> vld;    typedef vector<pii> vpii; typedef vector<pll> vpll; typedef vector<vector<int> > vvi;
-const int INF = (0x7FFFFFFFL); const ll INFF = (0x7FFFFFFFFFFFFFFFL); const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const int MOD = 1e9 + 7;       const int MODD = 998244353;            const string alphabet = "abcdefghijklmnopqrstuvwxyz";
-const double PI = acos(-1.0);  const double EPS = 1e-9;               const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-int dx[9] = { 1, 0, -1,  0,  1, -1, -1, 1, 0 };
-int dy[9] = { 0, 1,  0, -1, -1, -1,  1, 1, 0 };
+
+/* TYPE */
+typedef long long ll;       typedef long double ld;
+typedef pair<int, int> pii; typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;   typedef vector<pll> vpll;
+typedef vector<int> vi;     typedef vector<ll> vl;
+typedef vector<string> vst; typedef vector<bool> vb;
+typedef vector<ld> vld;     typedef vector<vector<int>> vvi;
+template<typename T, typename Cmp = less<>> using prique = priority_queue<T, vector<T>, Cmp>;
+template<typename T> using prique_r = prique<T, greater<>>;
+/* CONSTANT */
 #define ln '\n'
-#define scnaf scanf
-#define sacnf scanf
-#define sancf scanf
-#define SS(type, ...)type __VA_ARGS__;MACRO_VAR_Scan(__VA_ARGS__);
-template<typename T> void MACRO_VAR_Scan(T& t){cin >> t;}template<typename First, typename...Rest> void MACRO_VAR_Scan(First& first, Rest&...rest){cin >> first;MACRO_VAR_Scan(rest...);}
-#define SV(type,c,n) vector<type> c(n);for(auto& i:c)cin >> i;
-#define SVV(type,c,n,m) vector<vector<type>> c(n,vector<type>(m));for(auto& r:c)for(auto& i:r)cin >> i;
-template<class T,class U>ostream &operator<<(ostream &o,const pair<T,U>&j){o<<"{"<<j.first<<", "<<j.second<<"}";return o;}
-template<class T,class U>ostream &operator<<(ostream &o,const map<T,U>&j){o<<"{";for(auto t=j.begin();t!=j.end();++t)o<<(t!=j.begin()?", ":"")<<*t;o<<"}";return o;}
-template<class T>ostream &operator<<(ostream &o,const set<T>&j){o<<"{";for(auto t=j.begin();t!=j.end();++t)o<<(t!=j.begin()?", ":"")<<*t;o<<"}";return o;}
-template<class T>ostream &operator<<(ostream &o,const multiset<T>&j){o<<"{";for(auto t=j.begin();t!=j.end();++t)o<<(t!=j.begin()?", ":"")<<*t;o<<"}";return o;}
-template<class T>ostream &operator<<(ostream &o,const vector<T>&j){o<<"{";for(int i=0;i<(int)j.size();++i)o<<(i>0?", ":"")<<j[i];o<<"}";return o;}
-inline int print(void){cout << endl; return 0;}
-template<class Head> int print(Head&& head){cout << head;print();return 0;} template<class Head,class... Tail> int print(Head&& head,Tail&&... tail){cout<<head<<" ";print(forward<Tail>(tail)...);return 0;}
+const int INF = 1 << 30;    const ll INFF = 1LL << 60;  const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const int MOD = 1e9 + 7;    const int MODD = 998244353; const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+const double EPS = 1e-9;    const ld PI = 3.14159265358979323846264338327950288;
+const int dx[] = { 1, 0, -1,  0,  1, -1, -1, 1, 0 };
+const int dy[] = { 0, 1,  0, -1, -1, -1,  1, 1, 0 };
+/* CONTAINER */
+#define PB              emplace_back
+#define ALL(v)          (v).begin(), (v).end()
+#define RALL(v)         (v).rbegin(), (v).rend()
+#define SORT(v)         sort(ALL(v))
+#define RSORT(v)        sort(RALL(v))
+#define LESS(x, val)    (lower_bound(x.begin(), x.end(), val) - x.begin())
+#define LEQ(x, val)     (upper_bound(x.begin(), x.end(), val) - x.begin())
+#define GREATER(x, val) (int)(x).size() - LEQ((x), (val))
+#define GEQ(x, val)     (int)(x).size() - LESS((x), (val))
+#define UNIQUE(v)       sort(ALL(v)); (v).erase(unique(ALL(v)), (v).end())
+template<typename T> vector<T> make_v(size_t a) { return vector<T>(a); }
+template<typename T, typename... Ts> auto make_v(size_t a, Ts... ts) { return vector<decltype(make_v<T>(ts...))>(a, make_v<T>(ts...)); }
+template<typename T, typename U, typename... V> enable_if_t<is_same<T, U>::value != 0> fill_v(U &u, const V... v) { u = U(v...); }
+template<typename T, typename U, typename... V> enable_if_t<is_same<T, U>::value == 0> fill_v(U &u, const V... v) { for (auto &e : u) fill_v<T>(e, v...); }
+/* LOOP */
+#define _overload3(_1, _2, _3, name, ...) name
+#define _REP(i, n)      REPI(i, 0, n)
+#define REPI(i, a, b)   for (ll i = (ll)a; i < (ll)b; ++i)
+#define REP(...)        _overload3(__VA_ARGS__, REPI, _REP,)(__VA_ARGS__)
+#define _RREP(i, n)     RREPI(i, n, 0)
+#define RREPI(i, a, b)  for (ll i = (ll)a; i >= (ll)b; --i)
+#define RREP(...)       _overload3(__VA_ARGS__, RREPI, _RREP,)(__VA_ARGS__)
+#define EACH(e, v)      for (auto& e : v)
+#define PERM(v)         sort(ALL(v)); for (bool c##p = true; c##p; c##p = next_permutation(ALL(v)))
+/* INPUT */
+template<typename T> void SSS(T& t) { cin >> t; }
+template<typename Head, typename... Tail> void SSS(Head&& head, Tail&&... tail) { cin >> head; SSS(tail...); }
+#define SS(T, ...)      T __VA_ARGS__; SSS(__VA_ARGS__);
+#define SV(T, v, n)     vector<T> v(n); for (auto& i : v) cin >> i;
+#define SVV(T, v, n, m) vector<vector<T>> v(n, vector<T>(m)); for (auto& r : v) for (auto& i : r) cin >> i;
+/* OUTPUT */
+// PROTOTYPE DECLARATION
+template<typename T, typename U> ostream &operator<<(ostream &os, const pair<T, U> &j);
+template<typename... T> ostream &operator<<(ostream &os, const tuple<T...> &t);
+template<class C, enable_if_t<!is_same<C, string>::value, decltype(declval<const C &>().begin(), nullptr)> = nullptr> ostream& operator<<(ostream &os, const C &c);
+template<typename T> ostream &operator<<(ostream &os, const stack<T> &j);
+template<typename T> ostream &operator<<(ostream &os, const queue<T> &j);
+template<typename T, typename C, typename Cmp> ostream &operator<<(ostream &os, const priority_queue<T, C, Cmp> &j);
+// IMPLEMENTATION
+template<typename T, typename U> ostream &operator<<(ostream &os, const pair<T, U> &j) { return os << '{' << j.first << ", " << j.second << '}'; }
+template<size_t num = 0, typename... T> enable_if_t<num == sizeof...(T)> PRINT_TUPLE(ostream &os, const tuple<T...> &t) {}
+template<size_t num = 0, typename... T> enable_if_t<num <  sizeof...(T)> PRINT_TUPLE(ostream &os, const tuple<T...> &t) { os << get<num>(t); if (num + 1 < sizeof...(T)) os << ", "; PRINT_TUPLE<num + 1>(os, t); }
+template<typename... T> ostream &operator<<(ostream &os, const tuple<T...> &t) { PRINT_TUPLE(os << '{', t); return os << '}'; }
+template<class C, enable_if_t<!is_same<C, string>::value, decltype(declval<const C &>().begin(), nullptr)>> ostream& operator<<(ostream &os, const C &c) { os << '{'; for (auto it = begin(c); it != end(c); it++) { if (begin(c) != it) os << ", "; os << *it; } return os << '}'; }
+template<typename T> ostream &operator<<(ostream &os, const stack<T> &j) { deque<T> d; for (auto c = j; !c.empty(); c.pop()) d.push_front(c.top());  return os << d; }
+template<typename T> ostream &operator<<(ostream &os, const queue<T> &j) { deque<T> d; for (auto c = j; !c.empty(); c.pop()) d.push_back(c.front()); return os << d; }
+template<typename T, typename C, typename Cmp> ostream &operator<<(ostream &os, const priority_queue<T, C, Cmp> &j) { deque<T> d; for (auto c = j; !c.empty(); c.pop()) d.push_front(c.top());  return os << d; }
+// OUTPUT FUNCTION
+template<typename T> int PV(T &v) { int sz = v.size(); for (int i = 0; i < sz; ++i) cout << v[i] << " \n"[i == sz - 1]; return 0; }
+inline int print() { cout << endl; return 0; }
+template<typename Head> int print(Head&& head){ cout << head; return print(); }
+template<typename Head, typename... Tail> int print(Head&& head, Tail&&... tail) { cout << head << " "; return print(forward<Tail>(tail)...); }
 #ifdef LOCAL
-inline int dump(void){cerr << endl; return 0;}
-template<class Head> int dump(Head&& head){cerr << head;dump();return 0;}
-template<class Head,class... Tail> int dump(Head&& head,Tail&&... tail){cerr<<head<<" ";dump(forward<Tail>(tail)...);return 0;}
-#define debug(...) do{cerr<<__LINE__<<":\t"<<#__VA_ARGS__<<" = ";dump(__VA_ARGS__);}while(0)
-#define ER(x)  cerr << #x << " = " << (x) << endl;
-#define ERV(v) {cerr << #v << " : ";for(const auto& xxx : v){cerr << xxx << " ";}cerr << "\n";}
+inline void dump() { cerr << endl; }
+template<typename Head> void dump(Head&& head) { cerr << head; dump(); }
+template<typename Head, typename... Tail> void dump(Head&& head, Tail&&... tail) { cerr << head << ", "; dump(forward<Tail>(tail)...); }
+#define debug(...) do {cerr << __LINE__ << ":\t" << #__VA_ARGS__ << " = "; dump(__VA_ARGS__); } while (false)
 #else
 #define dump(...)
 #define debug(...)
-#define ER(x)
-#define ERV(v)
 #endif
-template<typename T> void PA(T &a){int ASIZE=sizeof(a)/sizeof(a[0]);for(int ii=0;ii<ASIZE;++ii){cout<<a[ii]<<" \n"[ii==ASIZE-1];}}
-template<typename T> void PV(T &v){int VSIZE=v.size();for(int ii=0;ii<VSIZE;++ii){cout<<v[ii]<<" \n"[ii==VSIZE-1];}}
-inline int YES(bool x){cout<<((x)?"YES":"NO")<<endl;return 0;} inline int Yes(bool x){cout<<((x)?"Yes":"No")<<endl;return 0;}  inline int yes(bool x){cout<<((x)?"yes":"no")<<endl;return 0;}
-inline int yES(bool x){cout<<((x)?"yES":"nO")<<endl;return 0;} inline int Yay(bool x){cout<<((x)?"Yay!":":(")<<endl;return 0;}
-template<typename A,typename B> void sankou(bool x,A a,B b){cout<<((x)?(a):(b))<<endl;}
-#define _overload3(_1,_2,_3,name,...) name
-#define _REP(i,n) REPI(i,0,n)
-#define REPI(i,a,b) for(ll i=ll(a);i<ll(b);++i)
-#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
-#define _RREP(i,n) RREPI(i,n,0)
-#define RREPI(i,a,b) for(ll i=ll(a);i>=ll(b);--i)
-#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
-#define EACH(e,v) for(auto& e : v)
-#define PERM(v) sort((v).begin(),(v).end());for(bool c##p=1;c##p;c##p=next_permutation((v).begin(),(v).end()))
-#define ADD(a,b) a=(a+ll(b))%MOD
-#define MUL(a,b) a=(a*ll(b))%MOD
-inline ll MOP(ll x,ll n,ll m=MOD){ll r=1;while(n>0){if(n&1)(r*=x)%=m;(x*=x)%=m;n>>=1;}return r;}
-inline ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}inline ll lcm(ll a,ll b){return a*b/gcd(a,b);}inline ll POW(ll a,ll b){ll c=1ll;do{if(b&1)c*=1ll*a;a*=1ll*a;}while(b>>=1);return c;}
-template<typename T,typename A,typename B> inline bool between(T x,A a,B b) {return ((a<=x)&&(x<b));}template<class T> inline T sqr(T x){return x*x;}
-template<typename A,typename B> inline bool chmax(A &a,const B &b){if(a<b){a=b;return 1;}return 0;}
-template<typename A,typename B> inline bool chmin(A &a,const B &b){if(a>b){a=b;return 1;}return 0;}
-#define tmax(x,y,z) max((x),max((y),(z)))
-#define tmin(x,y,z) min((x),min((y),(z)))
-#define PB emplace_back
-#define MP make_pair
-#define MT make_tuple
-#define all(v) (v).begin(),(v).end()
-#define rall(v) (v).rbegin(),(v).rend()
-#define SORT(v) sort((v).begin(),(v).end())
-#define RSORT(v) sort((v).rbegin(),(v).rend())
-#define EXIST(s,e) (find((s).begin(),(s).end(),(e))!=(s).end())
-#define EXISTST(s,c) (((s).find(c))!=string::npos)
-#define POSL(x,val) (lower_bound(x.begin(),x.end(),val)-x.begin())
-#define POSU(x,val) (upper_bound(x.begin(),x.end(),val)-x.begin())
-#define GEQ(x,val) (int)(x).size() - POSL((x),(val))
-#define GREATER(x,val) (int)(x).size() - POSU((x),(val))
-#define LEQ(x,val) POSU((x),(val))
-#define LESS(x,val) POSL((x),(val))
-#define SZV(a) int((a).size())
-#define SZA(a) sizeof(a)/sizeof(a[0])
-#define ZERO(a) memset(a,0,sizeof(a))
-#define MINUS(a) memset(a,0xff,sizeof(a))
-#define MEMINF(a) memset(a,0x3f,sizeof(a))
-#define FILL(a,b) memset(a,b,sizeof(a))
-#define UNIQUE(v) sort((v).begin(),(v).end());(v).erase(unique((v).begin(),(v).end()),(v).end())
-struct abracadabra{
-    abracadabra(){
-        cin.tie(0); ios::sync_with_stdio(0);
+/* OTHER */
+#define MP              make_pair
+#define MT              make_tuple
+#define tmax(x, y, z)   max((x), max((y), (z)))
+#define tmin(x, y, z)   min((x), min((y), (z)))
+template<typename T, typename A, typename B> inline bool between(T x, A a, B b) { return ((a <= x) && (x < b)); }
+template<typename A, typename B> inline bool chmax(A &a, const B &b) { if (a < b) { a = b; return true; } return false; }
+template<typename A, typename B> inline bool chmin(A &a, const B &b) { if (a > b) { a = b; return true; } return false; }
+inline ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+inline ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+inline ll POW(ll a, ll b)                    { ll r = 1; do { if (b & 1)  r *= a;        a *= a; }       while (b >>= 1); return r; }
+inline ll MOP(ll a, ll b, const ll &m = MOD) { ll r = 1; do { if (b & 1) (r *= a) %= m; (a *= a) %= m; } while (b >>= 1); return r; }
+struct abracadabra {
+    abracadabra() {
+        cin.tie(nullptr); ios::sync_with_stdio(false);
         cout << fixed << setprecision(20);
         cerr << fixed << setprecision(5);
     };
