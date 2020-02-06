8515e95bcee36f3f7f00c21e316e2e9475721680
morioprog <morio.prog@gmail.com>
2020-02-06 15:19:25 +0900

    Verify test

---
+++ DataStructure/ab.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    cout << A + B << endl;
+}
