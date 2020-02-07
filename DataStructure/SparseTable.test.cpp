c4aaeaf00ff44d5ae135da460434340a8679ed9f
morioprog <morio.prog@gmail.com>
2020-02-07 23:26:08 +0900

    add test : SparseTable

---
+++ DataStructure/SparseTable.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "SparseTable.cpp"
+
+int main() {
+    int N, Q;
+    cin >> N >> Q;
+
+    vector<int> A(N);
+    for (auto& e: A) cin >> e;
+    SparseTable<int> st(A);
+
+    while (Q--) {
+        int L, R;
+        cin >> L >> R;
+        cout << st.query(L, R) << endl;
+    }
+}
