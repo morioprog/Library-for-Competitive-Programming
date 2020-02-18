b0bbc637586a977b05f98bd8018a06321450df77
morioprog <morio.prog@gmail.com>
2020-02-18 14:42:09 +0900

    edit Meguru.cpp

--- Algorithm/Meguru.cpp
+++ Algorithm/Meguru.cpp
@@ -1,9 +1,5 @@
 long long ok = $1, ng = $2;
 while (abs(ok - ng) > 1) {
     long long mid = (ok + ng) >> 1;
-    if (check(mid)) {
-        ok = mid;
-    } else {
-        ng = mid;
-    }
+    (check(mid) ? ok : ng) = mid;
 }
